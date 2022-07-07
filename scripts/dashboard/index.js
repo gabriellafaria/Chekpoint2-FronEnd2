b11114ec6bcb9cfcf95366cd1a995791cfdb12d2
GabriellaFaria <gabisouf@gmail.com>
2022-07-06 21:08:46 -0300

    rewriting editTasks

--- scripts/dashboard/index.js
+++ scripts/dashboard/index.js
@@ -44,7 +44,7 @@ function createTaskHtml(taskData, isCompleted) {
         <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="25" height="25" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="white" d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093l3.473-4.425a.267.267 0 0 1 .02-.022z"/></svg>
       </div>
       <div class="descricao">
-        <p class="nome">${taskData.description}</p>
+        <p class="nome" id="task-${taskData.id}">${taskData.description}</p>
         <p class="timestamp">Criada em: ${dateFormat(taskData.createdAt)}</p>
         <span class="delete" onclick="deleteTask(${taskData.id})"><img src="./assets/delete.png" alt="Deletar task imagem"></span>
         <span class="edit" onclick="editTask(${taskData.id})"><img src="./assets/editar.png" alt="Editar task"></span>
@@ -164,25 +164,56 @@ function deleteTask(e) {
 }
 
 function editTask(id) {
-  // const logDescription = document.querySelector('.nome').textContent;
-  const newDescription = prompt('Qual a nova decrição?');
+  const logDescription = document.getElementById(`task-${id}`)
 
-  task.description = newDescription;
-  fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, {
-    method: 'PUT',
-    headers: {
-      Authorization: token,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(task),
+  const edit = `
+  <input id="name" placeholder="Insira mais de 5 letras"></input>
+  <button id="sendButton" type="submit">
+  <img src="./assets/send.png" alt="Editar task imagem">
+  <button>
+  <button id="cancelButton" type="reset">
+  <img src="./assets/cancel.png" alt="Cancelar task imagem">
+  <button> 
+`
+
+ let oldLogDescription = logDescription.innerHTML;
+ logDescription.innerHTML = edit;
+
+  const sendButton = document.getElementById('sendButton');
+  const editValidation = document.getElementById('editValidation');
+  const editDescription = document.getElementById('name');
+  const cancelButton = document.getElementById('cancelButton');
+
+  sendButton.addEventListener('click', () => {
+    if (editDescription.value.length > 5) {
+      task.description = editDescription.value;
+
+      fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, {
+        method: 'PUT',
+        headers: {
+          Authorization: token,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(task),
+      })
+        .then((response) => response.json())
+        .then((_data) => {
+          pendingTask.innerHTML = '';
+          completedTask.innerHTML = '';
+          renderTasks();
+        });
+    } else {
+      const call = eventsTasks(editDescription.value);
+
+      editValidation.innerHTML = call;
+      editDescription.style.borderBottom = call !== '' ? '1px solid #CC000E' : '';
+    }
+  });
+
+  cancelButton.addEventListener('click', () => {
+    logDescription.innerHTML = oldLogDescription
   })
-    .then((response) => response.json())
-    .then((_data) => {
-      pendingTask.innerHTML = '';
-      completedTask.innerHTML = '';
-      renderTasks();
-    });
-}
+};
 
 function completeTask(id) {
   fetch(`https://ctd-todo-api.herokuapp.com/v1/tasks/${id}`, {
