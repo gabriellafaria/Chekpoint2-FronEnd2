d26e4c58d7ba6b8f6724c0598556c32fd43f4c2f
Sabrina <freibergerss@gmail.com>
2022-07-12 21:13:04 -0300

    feat: :sparkles: Add Granim librarie

--- scripts/pages/tasks.js
+++ scripts/pages/tasks.js
@@ -188,3 +188,18 @@ function editTask(id) {
     logDescription.innerHTML = oldLogDescription
   })
 };
+
+var granimInstance = new Granim({
+  element: '#canvas-basic',
+  direction: 'top-bottom',
+  isPausedWhenNotInView: true,
+  states : {
+      "default-state": {
+          gradients: [
+              ['#ff9966', '#ff5e62'],
+              ['#00F260', '#0575E6'],
+              ['#e1eec3', '#f05053']
+          ]
+      }
+  }
+});
